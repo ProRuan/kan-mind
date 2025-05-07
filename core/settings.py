f572b9007742ca0d6dff77c6c90ce92c8de4a721
Your Name <you@example.com>
2025-05-07 14:56:53 +0200

    fixed admin panel login

--- core/settings.py
+++ core/settings.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',  # keep or delete?!
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
